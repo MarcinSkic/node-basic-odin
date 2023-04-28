ab1f98da6a09a81084a6bd38b1aee523000d705f
Marcin Skic <marcinskicoficjal@gmail.com>
2023-04-28 13:58:51 +0200

    Rewrite to use express framework

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,33 @@
-import http from "http";
-import url, { fileURLToPath } from "url";
+import express, { Express } from "express";
+import { fileURLToPath } from "url";
 import path from "path";
-import fs from "fs/promises";
 
+const app: Express = express();
 const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const htmlPath = path.join(__dirname, "../", "public");
 
-function router(req: http.IncomingMessage, res: http.ServerResponse) {
-    const targetUrl = new url.URL(req.url ?? "", `http:\\${req.headers.host}`);
-    console.log(targetUrl);
-    switch (targetUrl.pathname) {
-        case "/":
-            serveFile("index.html", res);
-            break;
-        case "/about":
-            serveFile("about.html", res);
-            break;
-        case "/contact-me":
-            serveFile("contact-me.html", res);
-            break;
-        case "/brew-coffee":
-            serveFile("418.html", res, 418);
-            break;
-        default:
-            serveFile("404.html", res, 404);
-            break;
-    }
-}
+app.get("/", (req, res) => {
+    res.sendFile(path.join(htmlPath, "index.html"));
+});
 
-async function serveFile(
-    filePath: string,
-    res: http.ServerResponse,
-    statusCode: number = 200
-) {
-    try {
-        const fileContents = await fs.readFile(path.join(htmlPath, filePath));
-        res.writeHead(statusCode, { "Content-Type": "text/html" });
-        res.end(fileContents);
-    } catch (error) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Server had a problem opening file");
-    }
-}
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(htmlPath, "about.html"));
+});
 
-const server = http.createServer(router);
+app.get("/contact-me", (req, res) => {
+    res.sendFile(path.join(htmlPath, "contact-me.html"));
+});
 
-server.listen(PORT, () => {
-    console.log("Server launched: http://localhost:3000");
+app.get("/brew-coffee", (req, res) => {
+    res.sendFile(path.join(htmlPath, "418.html"));
+});
+
+app.get("*", (req, res) => {
+    res.status(404).sendFile(path.join(htmlPath, "404.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
 });
